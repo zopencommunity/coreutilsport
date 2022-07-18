14939e568f866d969cd06d1272c0ba5bc5a352c3
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-18 08:46:52 -0400

    Add description to cicd.groovy script

--- cicd.groovy
+++ cicd.groovy
@@ -1,6 +1,6 @@
 node('linux') 
 {
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'coreutilsport'), string(name: 'DESCRIPTION', 'coreutilsport' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'coreutilsport'), string(name: 'DESCRIPTION', 'The GNU Core Utilities are the basic file, shell and text manipulation utilities of the GNU operating system.' )]
         }
 }
