5a3ba8b14ff0d922aaf93d821a378844469f425f
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-20 10:00:42 -0400

    Add full repo url

--- cicd.groovy
+++ cicd.groovy
@@ -10,6 +10,6 @@ node('linux')
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'coreutilsport'), string(name: 'PORT_DESCRIPTION', value: 'The GNU Core Utilities are the basic file, shell and text manipulation utilities of the GNU operating system.' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/coreutilsport.git'), string(name: 'PORT_DESCRIPTION', value: 'The GNU Core Utilities are the basic file, shell and text manipulation utilities of the GNU operating system.' )]
         }
 }
