8f3faa02555b29ae25e1df380962393f47a0eab2
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:20:26 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,10 +6,10 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/coreutilsport.git']]])
+                        userRemoteConfigs: [[url: 'https://github.com/zopencommunity/coreutilsport.git']]])
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/coreutilsport.git'), string(name: 'PORT_DESCRIPTION', value: 'The GNU Core Utilities are the basic file, shell and text manipulation utilities of the GNU operating system.' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/coreutilsport.git'), string(name: 'PORT_DESCRIPTION', value: 'The GNU Core Utilities are the basic file, shell and text manipulation utilities of the GNU operating system.' )]
         }
 }
